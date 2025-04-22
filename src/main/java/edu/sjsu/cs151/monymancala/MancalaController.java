469d0ae0ddb41a991c4435cdc0632424a5122393
Eddie Nguyen <tuong62642@gmail.com>
2025-04-22 12:00:35 -0700

    Create stub Controller class

---
+++ src/main/java/edu/sjsu/cs151/monymancala/MancalaController.java
@@ -0,0 +1,28 @@
+package edu.sjsu.cs151.monymancala;
+
+public class MancalaController {
+
+    private MancalaModel model;
+    private MancalaView view;
+
+    private int currentPlayer;
+    public MancalaController(MancalaModel model, MancalaView view) {
+        this.model = model;
+        this.view = view;
+
+        // Get buttons and link up here
+    }
+
+    public void endTurn() {
+        return;
+    }
+
+    public void undo() {
+        return;
+    }
+
+    public void gameOver() {
+        // Danny fill here;
+        return;
+    }
+}
