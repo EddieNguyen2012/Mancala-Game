db023760311198598588bd80304ea835b2e2cc0a
Eddie Nguyen <tuong62642@gmail.com>
2025-04-22 21:34:19 -0700

    Modify Model to handle undo and update initializeBoard()

--- src/main/java/edu/sjsu/cs151/monymancala/MancalaController.java
+++ src/main/java/edu/sjsu/cs151/monymancala/MancalaController.java
@@ -5,6 +5,8 @@ public class MancalaController {
     private MancalaModel model;
     private MancalaView view;
 
+    private int undoCount;
+
     private int currentPlayer;
     public MancalaController(MancalaModel model, MancalaView view) {
         this.model = model;
@@ -14,11 +16,18 @@ public class MancalaController {
     }
 
     public void endTurn() {
-        return;
+        undoCount = 0;
+        currentPlayer = (currentPlayer + 1) % 2; // Switch player indication
     }
 
     public void undo() {
-        return;
+        if (undoCount < 3) {
+            model.undo();
+            undoCount++;
+        }
+        else {
+            view.showErrorMessage("Maximum 3 undo per turn");
+        }
     }
 
     public void gameOver() {
