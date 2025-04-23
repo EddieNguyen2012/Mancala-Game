b8414d1afaa180cb02c29c333d6b84801ab0e85c
TheMarco408 <marco.lopez@sjsu.edu>
2025-04-23 00:39:56 -0700

    Update MancalaModel Constructor

--- src/main/java/edu/sjsu/cs151/monymancala/MancalaTester.java
+++ src/main/java/edu/sjsu/cs151/monymancala/MancalaTester.java
@@ -2,7 +2,7 @@ package edu.sjsu.cs151.monymancala;
 
 public class MancalaTester {
     public static void main(String[] args) {
-        MancalaModel model = new MancalaModel(5);
+        MancalaModel model = new MancalaModel();
         MancalaView view = new MancalaView(model);
         model.addChangeListener(view);
     }
