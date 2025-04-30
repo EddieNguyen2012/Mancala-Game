03db3347335108bb50a782de4ec362803307a7ef
VyeiVyos <129572729+VyeiVyos@users.noreply.github.com>
2025-04-29 22:48:55 -0700

    Add files via upload

---
+++ src/main/java/edu/sjsu/cs151/monymancala/OceanStyle.java
@@ -0,0 +1,48 @@
+package edu.sjsu.cs151.monymancala;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import javax.swing.*;
+
+public class OceanStyle implements BoardStyle{
+    
+    private Image board;
+    private Image mancala;
+    private Image pit;
+    private Image stones;
+    private Color labelColor;
+
+    public OceanStyle() {
+        board = new ImageIcon("Images/OceanBoard.png").getImage();
+        pit = new ImageIcon("Images/OceanPit.png").getImage();
+        stones = new ImageIcon("Images/SandDollar.png").getImage();
+        mancala = new ImageIcon("Images/BeachMancala.png").getImage();
+        this.labelColor = Color.WHITE;
+    }
+
+    @Override
+    public void drawPit(Graphics2D g2, int x, int y, int width, int height) {
+        g2.drawImage(pit, x, y, width, height, null);
+    }
+
+    @Override
+    public void drawMancala(Graphics2D g2, int x, int y, int width, int height) {
+        g2.drawImage(mancala, x, y, width, height, null);
+    }
+
+    @Override
+    public void drawStone(Graphics2D g2, int x, int y, int size) {
+        g2.drawImage(stones, x, y, size, size, null);
+    }
+
+    @Override
+    public void drawBackground(Graphics2D g2, int x, int y, int width, int height) {
+        g2.drawImage(board, x, y, width, height, null);
+    }
+
+    @Override
+    public Color getLabelColor() {
+        return labelColor;
+    }
+
+}
